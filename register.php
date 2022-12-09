0ae16c3a7f35592d58b35f42c3bb2486e2dcbec8
novakvova <novakvova@gmail>
2022-12-09 16:10:29 +0200

    add cropper register begin

--- register.php
+++ register.php
@@ -13,6 +13,19 @@
 <?php include "_header.php" ?>
 <div class="container">
     <h1 class="text-center">Реєстрація</h1>
+    <form class="col-md-6 offset-md-3" enctype="multipart/form-data" method="post">
+        <div class="mb-3">
+            <label for="image" class="form-label">
+                Фото
+                <img src="https://www.shutterstock.com/image-vector/hand-click-icon-vector-pointer-260nw-1901670688.jpg"
+                     alt=""
+                    width="300"
+                    style="cursor: pointer;">
+            </label>
+            <input type="file" class="form-control d-none"
+                   id="image" name="image">
+        </div>
+    </form>
 </div>
 
 <script src="js/bootstrap.bundle.min.js"></script>
