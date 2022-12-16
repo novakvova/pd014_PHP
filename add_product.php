a9e53d05cdd0b2445717c2778ab66b3612fcee47
novakvova <novakvova@gmail>
2022-12-16 16:24:17 +0200

    Add begin select images

--- add_product.php
+++ add_product.php
@@ -46,6 +46,7 @@ if($_SERVER['REQUEST_METHOD']=='POST')
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Головна стрінка</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -53,7 +54,7 @@ if($_SERVER['REQUEST_METHOD']=='POST')
 
 <div class="container">
     <h1 class="text-center">Додати продукт</h1>
-    <form class="col-md-6 offset-md-3" enctype="multipart/form-data" method="post">
+    <form enctype="multipart/form-data" method="post">
         <div class="mb-3">
             <label for="name" class="form-label">Назва</label>
             <input type="text" class="form-control" id="name" name="name">
@@ -63,8 +64,19 @@ if($_SERVER['REQUEST_METHOD']=='POST')
             <input type="text" class="form-control" id="price" name="price">
         </div>
         <div class="mb-3">
-            <label for="image" class="form-label">Фото</label>
-            <input type="file" class="form-control" id="image" name="image">
+            <div class="container">
+                <div class="row" id="list_images">
+
+
+
+                    <div class="col-md-2">
+                        <label for="image" style="cursor: pointer;" class="form-label text-success">
+                            <i class="fa fa-plus-square-o" style="font-size:120px" aria-hidden="true"></i>
+                        </label>
+                        <input type="file" class="form-control d-none" id="image" name="image" multiple>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Опис</label>
@@ -77,5 +89,80 @@ if($_SERVER['REQUEST_METHOD']=='POST')
 </div>
 
 <script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.6.2.min.js"></script>
+<script>
+
+    function uuidv4() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+    //window.onload
+    $(function () {
+//-----------------------SELECT IMAGES LIST---------------------------
+        const image = document.getElementById("image");
+        image.onchange = function (e)
+        {
+            const files = e.target.files;
+            for (let i=0; i<files.length;i++)
+            {
+                const reader = new FileReader();
+                reader.addEventListener('load', function() {
+                    const base64 = reader.result;
+                    const id = uuidv4();
+                    const data = `
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="fs-4 ms-2">
+                                    <label for="${id}">
+                                        <i class="fa fa-pencil" style="cursor: pointer;" aria-hidden="true"></i>
+                                    </label>
+                                    <input type="file" class="form-control d-none edit" id="${id}">
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-end fs-4 text-danger me-2 remove">
+                                    <i class="fa fa-times" style="cursor: pointer" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="${base64}" id="${id}_image" alt="photo" width="100%">
+                            <input type="hidden" id="${id}_file" value="${base64}">
+                        </div>
+                    `;
+                    const item = document.createElement('div');
+                    item.className="col-md-2 item-image";
+                    item.innerHTML=data;
+                    document.getElementById('list_images').prepend(item);
+                });
+                const file = files[i];
+                if(file)
+                    reader.readAsDataURL(file);
+            }
+            image.value="";
+        }
+//-----------------------REMOVE ITEM BY LIST---------------------------------------------
+        $("#list_images").on('click', '.remove', function() {
+            $(this).closest('.item-image').remove();
+        });
+//-----------------------CHANGE IMAGE LIST ITEM-------------------------------------
+        let edit_id=0;
+        const reader = new FileReader();
+        reader.addEventListener('load', ()=>{
+            const base64=reader.result;
+            document.getElementById(`${edit_id}_image`).src=base64;
+            document.getElementById(`${edit_id}_file`).value=base64;
+        });
+
+
+        $("#list_images").on('change', '.edit', function(e) {
+            edit_id=e.target.id;
+            const file = e.target.files[0];
+            reader.readAsDataURL(file);
+            this.value="";
+        });
+    });
+</script>
 </body>
 </html>
